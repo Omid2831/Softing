fa402d896e9913ab9180eb87c73e0a6ba5dabf33
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-29 20:15:52 +0100

    refactor: clean up HeaderContext component structure and remove unused CallToActionButton

--- src/features/about-us/context/HeaderContext.jsx
+++ src/features/about-us/context/HeaderContext.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
-import CallToActionButton from '../components/CallToActionButton';
+
 const HeaderContext = () => {
-  return (
-    <div className='flex flex-col px-3 py-6'>
-      <div className='mb-6'>
-        <h4 className='font-bold text-md text-blue-300'>STORY ABOUT US</h4>
-        <h2 className='text-3xl font-bold'>
-          Create your app page
-          <br />
-          with expert developer
-        </h2>
-        <p className='text-body text-gray-600 mt-4'>
-          Both rest of know draw fond post as. It agreement defective to excellent. Feebly do <br />
-          engage of narrow. Extensive repulsive belonging depending if promotion be <br /> zealously as.
-          Preference inquietude ask now are dispatched led appearance.
-          Small <br /> meant in so doubt hopes.
-        </p>
-      </div>
-      <CallToActionButton />
-      
-      {/** Head breaker  */}
-      <hr className='mt-6 border-0.5 border-gray-300' />
-    </div>
-  )
+    return (
+        <div className='mb-6'>
+            <h4 className='font-bold text-md text-blue-300'>STORY ABOUT US</h4>
+            <h2 className='text-3xl font-bold'>
+                Create your app page
+                <br />
+                with expert developer
+            </h2>
+            <p className='text-body text-gray-600 mt-4'>
+                Both rest of know draw fond post as. It agreement defective to excellent. Feebly do <br />
+                engage of narrow. Extensive repulsive belonging depending if promotion be <br /> zealously as.
+                Preference inquietude ask now are dispatched led appearance.
+                Small <br /> meant in so doubt hopes.
+            </p>
+        </div>
+    );
 }
 
 export default HeaderContext
