3fe6f71494d57a7415d614bbbb2fe34a6495a437
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-29 09:54:35 +0100

    fix: reverting the svg to png based file

--- src/components/Slider/data/sliderData.js
+++ src/components/Slider/data/sliderData.js
@@ -6,37 +6,37 @@ export const data = {
             {
                 "id": 0,
                 "title": "Marketing",
-                "icon": "marketing.svg",
+                "icon": "marketing.png",
                 "description": "Boost your brand's online presence with our comprehensive marketing service. From SEO to social media, we've got you covered. Contact us today!"
             },
             {
                 "id": 1,
                 "title": "Consultancy",
-                "icon": "consultancy.svg",
+                "icon": "consultancy.png",
                 "description": "Maximize your marketing potential with our expert consultancy service. From strategy to execution, we'll help you achieve your business goals."
             },
             {
                 "id": 2,
                 "title": "Effective",
-                "icon": "effective.svg",
+                "icon": "effective.png",
                 "description": "Our marketing service delivers results. From increased brand awareness to higher conversion rates, we'll help take your business to the next level."
             },
             {
                 "id": 3,
                 "title": "Strategy",
-                "icon": "strategy.svg",
+                "icon": "strategy.png",
                 "description": "Through targeted campaigns and data-driven insights, our main marketing strategy ensures maximum ROI for your business. Let's get started!"
             },
             {
                 "id": 4,
                 "title": "Agile",
-                "icon": "agile.svg",
+                "icon": "agile.png",
                 "description": "Our agile marketing methodology allows us to pivot quickly and adapt to changing market conditions, ensuring your brand stays ahead of the competition."
             },
             {
                 "id": 5,
                 "title": "Support",
-                "icon": "support.svg",
+                "icon": "support.png",
                 "description": "Our support service is available 24/7 to address any questions or concerns you may have. We're here to support your marketing success."
             }
         ]
