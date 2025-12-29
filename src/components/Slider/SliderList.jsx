aa09d638721a0f8ed6d058d27674633af3a40721
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-29 10:49:12 +0100

    feat: restructure SliderList component for improved layout and interaction

--- src/components/Slider/SliderList.jsx
+++ src/components/Slider/SliderList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import BannerActionButton from '../BannerActionButton';
 import '../../styles/slider-list.css';
+import PlayOverlay from '../PlayOverlay';
 
 const SliderList = () => {
     return (
         <section className='slider-section border-4 border-gray-600'>
             <div className="content flex flex-col md:flex-row items-center justify-between px-8 py-16 max-w-7xl mx-auto">
                 {/* Left: Text */}
-                <div className="max-w-xl">
+                <div >
                     <h1 className="font-bold text-3xl md:text-5xl mb-6">
                         We're Building <span className="text-blue-400">Software</span> <br /> For You
                     </h1>
@@ -17,7 +18,10 @@ const SliderList = () => {
                         Latter remark hunted enough vulgar say man. <br />
                         Sitting hearted on it without me.
                     </p>
-                    <BannerActionButton />
+                    <div className='flex flex-row gap-2 px-6 py-4'>
+                        <BannerActionButton />
+                        <PlayOverlay />
+                    </div>
                 </div>
                 {/* Right: Illustration */}
                 <div className="mt-10 md:mt-0 md:ml-10">
