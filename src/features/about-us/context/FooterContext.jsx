cb3292814c1eb83b881cb14f8e77b5a679235d1b
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-29 20:15:46 +0100

    feat: add FooterContext component with content and styling

---
+++ src/features/about-us/context/FooterContext.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FooterContext = () => {
+  return (
+    <section className='flex flex-col px-4 py-6 border-4 border-dashed border-gray-400'>
+        {/** Footer heading */}
+        <h2 className='text-2xl font-medium md:font-bold lg:font-extrabold mb-4 text-left md:text-left lg:text-left'>Why Choose Us</h2>
+        <ul className='list-disc px-6 py-4'>
+            <li>Elderly Is Detract Tedious Assured Private</li>
+            <li>Do Travelling Companions Contrasted</li>
+            <li>Mistress Strongly Remember Up</li>
+            <li>Branched Is On An Ecstatic Directly It</li>
+        </ul>
+    </section>
+  )
+}
+
+export default FooterContext
