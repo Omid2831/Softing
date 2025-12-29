e97eb4c8ed2ad5bad370f47debeb0597ee8af07d
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-29 19:38:46 +0100

    feat: add highlight-card data structure for feature display

---
+++ src/features/about-us/CardList/highlight-card.js
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        id: 1,
+        icon: 'ticket',
+        title: 'Ticket Management',
+        
+    },
+    {
+        id: 2,
+        icon: 'message',
+        title: 'Live Messaging',
+    },
+    {
+        id: 3,
+        icon: 'email',
+        title: 'Email workflow',
+    },
+    {
+        id: 4,
+        icon: 'file',
+        title: 'File Upload',
+    },
+];
